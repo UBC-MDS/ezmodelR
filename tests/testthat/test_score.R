2ceadb247210ce95e5266c1cd3148cea2a220bd5
tylercroberts <tcroberts@live.ca>
2018-03-03 18:11:42 -0800

    Add type check tests

--- tests/testthat/test_score.R
+++ tests/testthat/test_score.R
@@ -6,7 +6,11 @@ test_that("score() returns a function" {
 })
 
 test_that("score() returns an error if a supported model type is not passed"{
+  expect_warning(score("hello",'mse'), "Model specified must be one supported by the 'caret' package")
+})
 
+test_that("score() returns an error if a supported score type is not passed"{
+  expect_warning(score("rf",'goodbye'), "Score type specified must be one of 'mse', 'accuracy', 'r2', 'adj_r2', or 'roc'")
 })
 
 test_that("score() using mse returns a double"{
